6ed0744c78ff36956fa647b9066ef75ff9e0d9a7
Luis Pineda <idv17c.lpineda@gmail.com>
2019-01-26 03:01:39 -0600

    hub world
    se hicieron las promeras pruebas con los fondos y el movimiento del murcielago

---
+++ Vampire Date/Assets/Scenes/Scripts/CameraFollow.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Camera Cam;
+
+    // Variables
+    public Transform target;
+    public Vector3 offset;
+
+    // The speed of the camera
+    public float m_smoothTime = 0.5f;
+
+    /* Zoom Variables */
+    private float m_minZoom = 40f;
+    private float m_maxZoom = 10f;
+    private float m_zoomLimiter = 50f;
+
+    /* Track the Velocity */
+    private Vector3 m_velocity;
+
+    // Use this for initialization
+    void Start()
+    {
+        Cam = GetComponent<Camera>();
+    }
+
+    void LateUpdate()
+    {
+        transform.position = new Vector3(target.position.x + offset.x, offset.y, offset.z - 10);
+        Zoom();
+    }
+
+    void Zoom()
+    {
+        float newZoom = Mathf.Lerp(m_maxZoom, m_minZoom, GetGreatesDistance() / m_zoomLimiter); // This interprets between 2 values depending on a 3rd value
+        Cam.fieldOfView = Mathf.Lerp(Cam.fieldOfView, newZoom, Time.deltaTime);
+    }
+
+    float GetGreatesDistance()
+    {
+        var bounds = new Bounds(target.position, Vector3.zero);
+        bounds.Encapsulate(target.position);
+
+        // We just want to return the width
+        return bounds.size.x;
+    }
+}
---
+++ Vampire Date/Assets/Scenes/Scripts/Player.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    BoxCollider2D m_collider;
+    SpriteRenderer m_sprite;
+    Rigidbody2D m_rigidBody;
+    Animator m_animator;
+
+    float m_elizabethEmpathy;
+    float m_miriamEmpathy;
+
+    Vector2 m_direction;
+    float m_speed;
+
+    void Start()
+    {
+        m_collider = GetComponent<BoxCollider2D>();
+        m_rigidBody = GetComponent<Rigidbody2D>();
+        m_sprite = GetComponent<SpriteRenderer>();
+        m_animator = GetComponent<Animator>();
+
+        m_elizabethEmpathy = 0.0f;
+        m_miriamEmpathy = 0.0f;
+
+        m_speed = 4;
+    }
+
+    void Movement()
+    {
+        m_direction.x = Input.GetAxis("Horizontal");
+        m_direction.y = Input.GetAxis("Vertical");
+        m_rigidBody.velocity = new Vector2(m_direction.x * m_speed, m_direction.y * m_speed);
+
+        if (m_direction.x < 0)
+        {
+            m_sprite.flipX = false;
+        }
+
+        if (m_direction.x > 0)
+        {
+            m_sprite.flipX = true;
+        }
+    }
+
+    void Update()
+    {
+        Movement();
+    }
+}
